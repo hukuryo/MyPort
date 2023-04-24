d1eb96218d559e98140bead9337027d09e48de7a
hukuryo <ryohei.1998.05.09@gmail.com>
2023-04-24 21:25:00 +0900

    投稿にIDをつけられるようにしました

--- backend/app.js
+++ backend/app.js
@@ -12,55 +12,72 @@ app.use(cors({
   credentials: true
 }));
 
-const lll = app.get('/api/port/get', (req, res) => {
-  try{
-      //データを取りだす
-      const bufferData = fs.readFileSync('ports.json')
-      // データを文字列に変換
-      const dataJSON = bufferData.toString()
-      //JSONのデータをJavascriptのオブジェクトに
-      const data = JSON.parse(dataJSON)
-      console.log(data)
-      res.send(data)
-  }catch(e){
-      console.log("JSONデータなし")
+// データの長さを取得する関数
+function getArrayLength() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('ports.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      // ファイルをJSONパースして配列に変換する
+      const arr = JSON.parse(data);
+      // 配列の長さを返す
+      resolve(arr.length);
+    });
+  });
+}
+
+app.get('/api/port/get', (req, res) => {
+  try {
+    //データを取りだす
+    const bufferData = fs.readFileSync('ports.json')
+    // データを文字列に変換
+    const dataJSON = bufferData.toString()
+    //JSONのデータをJavascriptのオブジェクトに
+    const data = JSON.parse(dataJSON)
+    res.send(data)
+  } catch(e) {
+    console.log("JSONデータなし")
   }
 });
 
 // POSTメソッドでフォームの値を受け取る
 app.post('/api/port/save', (req, res) => {
-    console.log(lll.length)
-    try{
+  try {
+    getArrayLength()
+      .then((arrayLength) => {
         fs.readFile('ports.json', 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          // ファイルをJSONパースして配列に変換する
+          let arr = JSON.parse(data);
+          console.log(arr)
+          // 新しいオブジェクトを作成して配列に追加する
+          arr.push({
+            id: arrayLength + 1,
+            PortName: req.body.PortName,
+            PortUrl: req.body.PortUrl,
+            PortContent: req.body.PortContent,
+          });
+          // 配列をJSON文字列に変換する
+          let newData = JSON.stringify(arr);
+          // writeFileメソッドで、JSONファイルに送られてきたデータを追加する。
+          fs.writeFile('ports.json', newData, 'utf8', (err) => {
             if (err) {
-                console.error(err);
-                return;
+              console.error(err);
+              return;
             }
-            // ファイルをJSONパースして配列に変換する
-            let arr = JSON.parse(data);
-            // 新しいオブジェクトを作成して配列に追加する
-            arr.push({ 
-              id: lll.length + 1,
-              PortName: req.body.PortName, 
-              PortUrl: req.body.PortUrl, 
-              PortContent: req.body.PortContent,
-            });
-            // 配列をJSON文字列に変換する
-            let newData = JSON.stringify(arr);
-            // writeFileメソッドで、JSONファイルに送られてきたデータを追加する。
-            fs.writeFile('ports.json', newData, 'utf8', (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                console.log('JSONデータを追加！');
-            });
+            console.log('JSONデータを追加！');
+          });
         });
-    }catch(e){
-        console.log("error")
-    }
+      })
+  } catch(e) {
+    console.log("error")
+  }
 });
-
 
 // 3000番ポートでサーバーを起動できるようにする。
 app.listen(3000, () => console.log('Server running on port 3000'));
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,62 +1,106 @@
 <template>
 <div class="container">
-<main>
-    <div class="py-5 text-center">    
+    <main>
+    <div class="py-5 text-center">
         <h2>ポートフォリオ保存ページ</h2>
         <p class="lead">作成したポートフォリオを保存する</p>
     </div>
     <div class="form-body">
         <div class="">
-            <form class="needs-validation" novalidate="" action="http://localhost:3000/api/port/save" method="POST">
-                <div class="row g-3">
-                    <!-- PortName -->
-                    <div class="col-12">
-                        <label for="port-name" class="form-label">ポートフォリオの名前</label>
-                        <div class="input-group has-validation">
-                            <input type="text" class="form-control" id="PortName" name="PortName" placeholder="PortFolioName" v-model="PortName">                
-                        </div>
-                    </div>
-                    <!-- PortUrl -->
-                    <div class="col-12">
-                        <label for="email" class="form-label">URL</label>
-                        <input type="email" class="form-control" name="PortUrl" placeholder="SiteURL" v-model="PortUrl">
-                    </div>
-                    <!-- PortContent -->
-                    <div class="col-12">
-                        <label class="form-label">説明</label>
-                        <textarea class="form-control" name="PortContent" placeholder="SiteExplanation" v-model="PortContent"></textarea>                
-                    </div>
+        <form
+            class="needs-validation"
+            novalidate=""
+            action="http://localhost:3000/api/port/save"
+            method="POST"
+        >
+            <div class="row g-3">
+            <!-- PortName -->
+            <div class="col-12">
+                <label for="port-name" class="form-label"
+                >ポートフォリオの名前</label
+                >
+                <div class="input-group has-validation">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="PortName"
+                    name="PortName"
+                    placeholder="PortFolioName"
+                    v-model="PortName"
+                />
                 </div>
-                <hr class="my-4">
-                <button class="w-100 btn btn-primary btn-lg" type="submit" @click="save">ポートフォリをクリップ</button>
-            </form>
+            </div>
+            <!-- PortUrl -->
+            <div class="col-12">
+                <label for="email" class="form-label">URL</label>
+                <input
+                type="email"
+                class="form-control"
+                name="PortUrl"
+                placeholder="SiteURL"
+                v-model="PortUrl"
+                />
+            </div>
+            <!-- PortContent -->
+            <div class="col-12">
+                <label class="form-label">説明</label>
+                <textarea
+                class="form-control"
+                name="PortContent"
+                placeholder="SiteExplanation"
+                v-model="PortContent"
+                ></textarea>
+            </div>
+            </div>
+            <hr class="my-4" />
+            <button
+            class="w-100 btn btn-primary btn-lg"
+            type="submit"
+            @click="save"
+            >
+            ポートフォリをクリップ
+            </button>
+        </form>
         </div>
     </div>
-</main>
-<button @click="reception">JSONデータをもらう</button>
+    </main>
+    <button @click="reception">JSONデータをもらう</button>
 </div>
 </template>
-
 <script>
-// import axios from 'axios'
+import axios from "axios";
+
 export default {
-    name: 'PortView',
-    data() {
-        return {
-            PortName: "",
-            PortUrl: "",
-            PortContent: ""
-        }
+name: "PortView",
+data() {
+    return {
+        PortName: "",
+        PortUrl: '',
+        PortContent: '',
+    };
+},
+methods: {
+    save() {
+    let portsContent = {
+        id: Math.floor(Math.random() * 1000), // ランダムなIDを生成
+        PortName: this.PortName,
+        PortUrl: this.PortUrl,
+        PortContent: this.PortContent,
+    };
+    axios.post("http://localhost:3000/api/port/save", portsContent)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        this.$router.push("/");
     },
-    methods: {
-        save(){
-            this.$router.push('/');
-        },
-    }
-}
+},
+};
 </script>
 <style scoped>
-    .form-body{
-        text-align: initial;
-    }
+.form-body {
+text-align: initial;
+}
 </style>
