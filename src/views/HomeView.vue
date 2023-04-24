34f64ea4ed3630e5b36b3bdc2a2ebbdd957e89f9
hukuryo <ryohei.1998.05.09@gmail.com>
2023-04-24 21:40:24 +0900

    ポートフォリオを一覧表示できるようにしました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,24 +4,10 @@
       
       <!-- <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" v-for="port in getPorts" :key="getPorts.id"> -->
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div class="col">
+        <div class="col" v-for="item in portsData" :key="item.id">
           <div class="card shadow-sm">
             <div class="card-body">
-              <p class="card-text">ポートフォリオのサイト名</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <router-link class="btn btn-sm btn-outline-secondary" to="/post/1">View</router-link>
-                  <router-link class="btn btn-sm btn-outline-secondary" to="/edit/:id">Edit</router-link>
-                </div>
-                <small class="text-muted">作成日</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <div class="card-body">
-              <p class="card-text">ポートフォリオのサイト名</p>
+              <p class="card-text">{{ item.PortName }}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <router-link class="btn btn-sm btn-outline-secondary" to="/post/1">View</router-link>
@@ -33,19 +19,34 @@
           </div>
         </div>
       </div>
+
     </div>
   </div>
   <button @click="reception">JSONデータをもらう</button>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-    name: 'PortView',
-    computed: {
-      // getPorts(){
-      //   this.$store.getters.reception
-      // }
+  name: 'PortView',
+  data(){
+    return {
+      portsData: {}
     }
+  },
+  created(){
+    this.getMessages()
+  },
+  methods: {
+    getMessages() {  
+      axios.get("http://localhost:3000/api/port/get")
+        .then(response => {
+          this.portsData = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+  }
 }
 </script>
