ead611612e7db07093dd069b6370969444561409
hukuryo <ryohei.1998.05.09@gmail.com>
2023-05-21 08:57:59 +0900

    ユーザーに紐づいたポートフォリオのみ表示するようにしました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,14 @@
   <div class="album py-5 bg-light">
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div class="col" v-for="item in portsData" :key="item.id">
+        <div class="col" v-for="item in userPortsData" :key="item.id">
           <div class="card shadow-sm">
             <div class="card-body">
               <p class="card-text">{{ item.PortName }}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <router-link :to= "{name: 'postDetail', params: {id: item.id}}" class="btn btn-sm btn-outline-secondary" >View</router-link>
-                  <router-link :to= "{name: 'postEdit', params: {id: item.id}}" class="btn btn-sm btn-outline-secondary" >Edit</router-link>
+                  <router-link :to="{ name: 'postDetail', params: { id: item.id }}" class="btn btn-sm btn-outline-secondary">View</router-link>
+                  <router-link :to="{ name: 'postEdit', params: { id: item.id }}" class="btn btn-sm btn-outline-secondary">Edit</router-link>
                 </div>
               </div>
             </div>
@@ -19,36 +19,37 @@
     </div>
   </div>
 </template>
-
 <script>
 import axios from "axios";
-const postUserLocalId = JSON.parse(localStorage.getItem("vuex"))
 
 export default {
-  name: 'PortView',
-  data(){
+  name: "PortView",
+  data() {
     return {
-      portsData: {}
-    }
+      userPortsData: [], // ログインしているユーザーの投稿データを保持する配列
+    };
   },
-  created(){
-    this.getPortFolio()
+  created() {
+    this.getPortFolio();
   },
   methods: {
-    getPortFolio() {  
-      axios.get("http://localhost:3000/api/port/get")
-        .then(response => {
-          for(let i = 0; i <= response.data.length - 1; i++){
-            if(postUserLocalId.id == response.data[i].postUserId){
-              this.portsData = response.data
-              console.log(this.portsData)
-            }
+    getPortFolio() {
+      const postUserLocalData = JSON.parse(localStorage.getItem("vuex"));
+      axios
+        .get("http://localhost:3000/api/port/get")
+        .then((response) => {
+          try {
+            this.userPortsData = response.data.filter(
+              (item) => item.postUserId === postUserLocalData.id
+            );
+          } catch (e) {
+            return;
           }
         })
-        .catch(error => {
-          console.log(error)
-        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-  }
-}
+  },
+};
 </script>
