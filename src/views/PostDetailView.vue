a0ad739f2ef80a393c0891161aae89011d305cfb
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-03 06:27:04 +0900

    詳細ページを改良中

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container">
+<!-- <div class="container">
     <main>
         <div class="py-5 text-center">
             <h2>ポートフォリオ詳細</h2>
@@ -26,6 +26,38 @@
         </div>
         <router-link class="btn btn-success" to="/">戻る</router-link>
     </main>
+</div> -->
+
+
+
+<div class="col-lg-8 mx-auto p-3 py-md-5">
+<header class="d-flex align-items-center pb-3 mb-5 border-bottom">
+    <span class="fs-4">ポートフォリオ詳細</span>
+</header>
+
+<main>
+
+    <div class="row g-5">
+    <div class="col-md-6">
+        <h2></h2>
+        <p>Ready to beyond the starter template? Check out these open source projects that you can quickly duplicate to a new GitHub repository.</p>
+        <ul class="icon-list">
+        <li><a href="https://github.com/twbs/bootstrap-npm-starter" rel="noopener" target="_blank">Bootstrap npm starter</a></li>
+        <li class="text-muted">Bootstrap Parcel starter (coming soon!)</li>
+        </ul>
+    </div>
+
+    <div class="col-md-6">
+        <h2>ポートフォリオ</h2>
+        <ul class="icon-list">
+        <li><a href="/docs/5.0/getting-started/introduction/">Bootstrap quick start guide</a></li>
+        <li><a href="/docs/5.0/getting-started/webpack/">Bootstrap Webpack guide</a></li>
+        <li><a href="/docs/5.0/getting-started/parcel/">Bootstrap Parcel guide</a></li>
+        <li><a href="/docs/5.0/getting-started/build-tools/">Contributing to Bootstrap</a></li>
+        </ul>
+    </div>
+    </div>
+</main>
 </div>
 </template>
 
